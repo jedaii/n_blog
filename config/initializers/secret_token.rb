3f54bbae77a40eac45527c0cc8c9359991ad7460
jedaii <a1exander@inbox.ru>
2013-06-08 00:38:22 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NBlog::Application.config.secret_token = 'c7fa58940eacfbf5e798487300281e6a476b56999f38601f42112b0a8f2b766872b465155f96d42543e65064dcad82b9ef818ab9b8b96cf895e5a3e141b4840b'
